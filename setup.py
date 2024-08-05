d9c8a8f6f699c4235a253e6838d1639acada1de2
Reshma <reshmanalage2706@gmail.com>
2024-08-05 14:02:22 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Reshma'
+author_email='reshmanalage2706@gmail.com',
+packages=find_packages()
+install_requires=get_requirements(requirement.txt)
+
+
+
+)
